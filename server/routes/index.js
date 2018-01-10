1c98bce8c0f63cf7f5b8708548d0dc3d9755ff5e
bozhi.lai <bozhi.lai@vipshop.com>
2018-01-10 17:20:54 +0800

    [A] Init Server

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	res.json({
+		code: 200, 
+		msg: 'Hello, Sally', 
+		data: null
+	}); 
+});
+
+module.exports = router;
