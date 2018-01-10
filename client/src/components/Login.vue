d903dc198db68328c7b5e023701625380bddf5ba
eczn <1975845422@qq.com>
2018-01-10 22:35:31 +0800

    [M] 添加样式 登陆界面

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="login-containner">
-		<div>login</div>
+		<input type="text" class="username" v-model="user.uname" />
+		<input type="text" class="pwd" v-model="user.pwd">
+		<button @click="toLogin">登陆</button>
 	</div>
 </template>
 
@@ -10,13 +12,20 @@ import http from '@/utils/http.client';
 export default {
 	name: 'login',
 	data: () => ({
-		
+		user: {
+			uname: '',
+			pwd: ''
+		}
 	}),
 	created(){
 		
 	},
 	methods: {
+		toLogin(){
+			http.post('/api/user/login', this.user).then(ok => {
 
+			}); 
+		}
 	}
 }
 </script>
